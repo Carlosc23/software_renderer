eb0b79d86f784399ec887679836fbf918096656b
Carlosc23 <cal15219@uvg.edu.gt>
2019-05-12 22:22:30 -0600

    feat: add coords for medium and low shots

--- SR6.py
+++ SR6.py
@@ -1,22 +1,38 @@
 from SR1 import SoftwareRender
 from obj_loader import Texture
 
+
 def medium():
-    x = SoftwareRender('out.bmp')
+    x = SoftwareRender('medium.bmp')
     x.glCreateWindow(800, 600)
     x.glViewPort(0, 0, 800, 600)
     x.glClear()
     t = Texture('./models/mushroom2.bmp')
-    x.load_img_texture2('./models/mushroom.obj', (16, 9, 0), (25, 25, 1), t, [0, 0, 1],[0,0,1],[0,0,0],[0,1,0])
+    x.load2("./models/mushroom.obj", (-0.05,-0.45,0), (1.2, 1.2, 1.2), (0, 0, 0), [0, 0.75, 1.5], [0.1, 0.5, 1], [0, 0.5, 0],
+            [0, 1, 0])
     x.glFinish()
 
-def low():
-    pass
 
-def dutch():
-    pass
+def low():
+    x = SoftwareRender('low.bmp')
+    x.glCreateWindow(800, 600)
+    x.glViewPort(0, 0, 800, 600)
+    x.glClear()
+    t = Texture('./models/mushroom2.bmp')
+    x.load2("./models/mushroom.obj", (-0.05,-0.45, 0), (0.8, 0.8, 0.8), (-1.15, 0.2, -0.05), [0, 0.75, 1.5], [0.1, 0.5, 1], [0,-0.3,0.2],
+            [0, 1, 0])
+    x.glFinish()
+
 
 def high():
-    pass
+    x = SoftwareRender('high.bmp')
+    x.glCreateWindow(800, 600)
+    x.glViewPort(0, 0, 800, 600)
+    x.glClear()
+    t = Texture('./models/mushroom2.bmp')
+    x.load2("./models/mushroom.obj", (0, 0, 0), (0.5,0.5,0.5), (0, 0, 0), [0, 0.75, 1.5], [0.1, 0.5, 1],[0, 0, 0],
+            [0, 1, 0])
+    x.glFinish()
 
-medium()
+
+low()
